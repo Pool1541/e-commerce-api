9933c568ce3cd6a8a0810e6766826a2d4f7c7887
Pool <pool_1541@hotmail.com>
2023-05-15 19:36:15 -0500

    Product schema created

---
+++ src/models/product.js
@@ -0,0 +1,35 @@
+const { Schema, model, Types } = require("mongoose");
+
+const ProducthSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: Types.ObjectId,
+    ref: "Category",
+  },
+  countInStock: {
+    type: Number,
+    required: true,
+  },
+});
+
+ProducthSchema.methods.toJSON = function () {
+  const { __v, ...rest } = this.toObject();
+  return rest;
+};
+
+module.exports = model("Product", ProducthSchema);
