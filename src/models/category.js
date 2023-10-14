3c27f033eb1225905952f72fe26072e8e5797206
Pool <pool_1541@hotmail.com>
2023-10-13 23:02:38 -0500

    refactor(models): update category model

--- src/models/category.js
+++ src/models/category.js
@@ -1,10 +1,14 @@
-const { Schema, model } = require("mongoose");
+const { Schema, model } = require('mongoose');
 
 const CategorySchema = new Schema({
   name: {
     type: String,
     required: true,
   },
+  image: {
+    type: String,
+    required: true,
+  },
 });
 
-module.exports = model("Category", CategorySchema);
+module.exports = model('Category', CategorySchema);
