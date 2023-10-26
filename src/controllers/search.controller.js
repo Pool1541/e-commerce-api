27f93e96428714c84eacb6bc5182e88ed7ba0592
Pool <pool_1541@hotmail.com>
2023-10-26 18:18:06 -0500

    feat(controller): update search controller

--- src/controllers/search.controller.js
+++ src/controllers/search.controller.js
@@ -6,7 +6,7 @@ const search = async (req = request, res = response) => {
 
   const regex = new RegExp(keyword, 'i');
   try {
-    const [count, results] = await Promise.all([
+    const [count, products] = await Promise.all([
       Product.countDocuments({
         $or: [{ title: regex }, { brand: { $regex: regex } }],
       }),
@@ -17,7 +17,7 @@ const search = async (req = request, res = response) => {
 
     res.json({
       count,
-      results,
+      products,
     });
   } catch (error) {
     console.log(error);
