7325b91f7551e9f6e1e84119050b88bdfe6fb4bd
pool_1541 <pool_1541@hotmail.com>
2023-11-25 23:43:18 -0500

    chore(format): format validateJWT file

--- src/middlewares/validateJWT.js
+++ src/middlewares/validateJWT.js
@@ -1,11 +1,12 @@
-const { response, request } = require("express");
-const jwt = require("jsonwebtoken");
-const User = require("../models/user");
+const { response, request } = require('express');
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
 
 const validateJWT = async (req = request, res = response, next) => {
-  const token = req.header("Authorization");
+  const token = req.header('Authorization');
+
   if (!token) {
-    return res.status(401).json({ error: ["No token provided"] });
+    return res.status(401).json({ error: ['No token provided'] });
   }
 
   try {
@@ -13,22 +14,18 @@ const validateJWT = async (req = request, res = response, next) => {
     const user = await User.findById(uid);
 
     if (!user) {
-      return res
-        .status(401)
-        .json({ error: ["Invalid token : user does not exist"] });
+      return res.status(401).json({ error: ['Invalid token : user does not exist'] });
     }
 
     if (!user.status) {
-      return res
-        .status(401)
-        .json({ error: ["Invalid token : user is not active"] });
+      return res.status(401).json({ error: ['Invalid token : user is not active'] });
     }
 
     req.user = user;
     next();
   } catch (error) {
     console.log(error);
-    return res.status(401).json({ error: ["Invalid token"] });
+    return res.status(401).json({ error: ['Invalid token'] });
   }
 };
 
