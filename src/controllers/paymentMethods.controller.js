df11ba34e1bd3b4493d2a23feef25a1423967782
pool_1541 <pool_1541@hotmail.com>
2024-02-26 22:35:31 -0500

    feat(controller): add createPaymentMethod controller

--- src/controllers/paymentMethods.controller.js
+++ src/controllers/paymentMethods.controller.js
@@ -21,7 +21,29 @@ const getPaymentMethod = async (req = request, res = response) => {
   }
 };
 
-const createPaymentMethod = async (req = request, res = response) => {};
+const createPaymentMethod = async (req = request, res = response) => {
+  const { cardName, cardNumber, expirationDate, securityCode, user } = req.body;
+
+  try {
+    const paymentMethod = new PaymentMethod({
+      cardName,
+      cardNumber,
+      expirationDate,
+      securityCode,
+      user,
+    });
+
+    await paymentMethod.save();
+
+    return res.status(201).json({
+      data: 'PaymentMethod created succesfuly',
+    });
+  } catch (error) {
+    return res.status(500).json({
+      error: ['Something went wrong'],
+    });
+  }
+};
 
 const deletePaymentMethod = (req = request, res = response) => {};
 
