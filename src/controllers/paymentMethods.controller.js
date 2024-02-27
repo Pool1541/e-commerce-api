afd3a290f64d89681630377dccd0865cf3517e3c
pool_1541 <pool_1541@hotmail.com>
2024-02-26 22:34:41 -0500

    feat(controller): add getPaymentMethod controller

--- src/controllers/paymentMethods.controller.js
+++ src/controllers/paymentMethods.controller.js
@@ -1,19 +1,33 @@
 const { request, response } = require('express');
+const PaymentMethod = require('../models/payment-method');
 
 const getPaymentMethodsByUser = (req = request, res = response) => {};
-const getPaymentMethod = (req = request, res = response) => {};
-const createPaymentMethod = (req = request, res = response) => {
-  const body = req.body;
 
-  console.log(body);
+const getPaymentMethod = async (req = request, res = response) => {
+  try {
+    const { id } = req.params;
+    const { _id: uid } = req.user;
 
-  res.status(201).json({
-    body,
-  });
+    const paymentMethod = await PaymentMethod.findById(id);
+
+    if (!paymentMethod) return res.status(404).json({ error: ['Payment method not found'] });
+
+    if (uid.toString() !== paymentMethod.user.toString())
+      return res.status(401).json({ error: ['Not authorized'] });
+
+    return res.status(200).json({ data: paymentMethod });
+  } catch (error) {
+    return res.status(500).json({ error: ['Something went wrong'] });
+  }
 };
+
+const createPaymentMethod = async (req = request, res = response) => {};
+
+const deletePaymentMethod = (req = request, res = response) => {};
 
 module.exports = {
   getPaymentMethodsByUser,
   getPaymentMethod,
   createPaymentMethod,
+  deletePaymentMethod,
 };
