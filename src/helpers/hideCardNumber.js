560747534baacbee27be35fb331b9bc4676a134e
pool_1541 <pool_1541@hotmail.com>
2024-02-29 00:12:50 -0500

    chore(helpers): add hideCardNumber function

---
+++ src/helpers/hideCardNumber.js
@@ -0,0 +1,17 @@
+const hideCardNumber = (number) => {
+  let length = number.length;
+
+  let firstNumbers = number.substring(0, 4);
+
+  let lastNumbers = number.substring(length - 4);
+
+  let asterisks = '*'.repeat(length - 8);
+
+  let result = firstNumbers + asterisks + lastNumbers;
+
+  return result;
+};
+
+module.exports = {
+  hideCardNumber,
+};
