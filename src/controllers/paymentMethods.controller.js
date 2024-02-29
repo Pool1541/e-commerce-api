defc011bbbfc9a588dd34527ae53b19af6960de7
pool_1541 <pool_1541@hotmail.com>
2024-02-29 00:14:29 -0500

    feat(controllers): add deletePaymentMethod and improve getPaymentMethod

--- src/controllers/paymentMethods.controller.js
+++ src/controllers/paymentMethods.controller.js
@@ -1,6 +1,7 @@
 const { request, response } = require('express');
 const PaymentMethod = require('../models/payment-method');
-const { encryptData } = require('../helpers/encryptData');
+const { encryptData, decryptData } = require('../helpers/crypto');
+const { hideCardNumber } = require('../helpers/hideCardNumber');
 
 const getPaymentMethodsByUser = (req = request, res = response) => {};
 
@@ -16,8 +17,19 @@ const getPaymentMethod = async (req = request, res = response) => {
     if (uid.toString() !== paymentMethod.user.toString())
       return res.status(401).json({ error: ['Not authorized'] });
 
-    return res.status(200).json({ data: paymentMethod });
+    let { _id, cardNumber, expirationDate } = paymentMethod;
+
+    cardNumber = hideCardNumber(decryptData([cardNumber])[0]);
+
+    return res.status(200).json({
+      data: {
+        _id,
+        cardNumber,
+        expirationDate,
+      },
+    });
   } catch (error) {
+    console.log(error);
     return res.status(500).json({ error: ['Something went wrong'] });
   }
 };
@@ -49,7 +61,24 @@ const createPaymentMethod = async (req = request, res = response) => {
   }
 };
 
-const deletePaymentMethod = (req = request, res = response) => {};
+const deletePaymentMethod = async (req = request, res = response) => {
+  try {
+    const { id } = req.params;
+    const { _id: uid } = req.user;
+
+    const paymentMethod = await PaymentMethod.findById(id);
+    if (!paymentMethod) return res.status(404).json({ error: ['Payment method not found'] });
+
+    if (uid.toString() !== paymentMethod.user.toString())
+      return res.status(401).json({ error: ['Not authorized'] });
+
+    await paymentMethod.deleteOne();
+    return res.status(204).json();
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: ['Something went wrong'] });
+  }
+};
 
 module.exports = {
   getPaymentMethodsByUser,
