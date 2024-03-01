6b1befccf099137765ffb5b3ba90b1f3db6b082b
pool_1541 <pool_1541@hotmail.com>
2024-02-29 23:39:53 -0500

    chore(helpers): format code

--- src/helpers/hideCardNumber.js
+++ src/helpers/hideCardNumber.js
@@ -1,12 +1,8 @@
 const hideCardNumber = (number) => {
   let length = number.length;
-
   let firstNumbers = number.substring(0, 4);
-
   let lastNumbers = number.substring(length - 4);
-
   let asterisks = '*'.repeat(length - 8);
-
   let result = firstNumbers + asterisks + lastNumbers;
 
   return result;
