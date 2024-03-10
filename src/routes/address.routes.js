de950f63db96a52c15bb1335467ebc2f27e07bec
pool_1541 <pool_1541@hotmail.com>
2024-03-10 18:51:57 -0500

    feat(routes): add patch route in address

--- src/routes/address.routes.js
+++ src/routes/address.routes.js
@@ -1,5 +1,5 @@
 const { Router } = require('express');
-const { getAddress, createAddress } = require('../controllers/address.controller');
+const { getAddress, createAddress, updateAddress } = require('../controllers/address.controller');
 const { check } = require('express-validator');
 
 const { validateJWT } = require('../middlewares/validateJWT');
@@ -110,6 +110,92 @@ router.post(
     validateInputs,
   ],
   createAddress
+);
+
+router.patch(
+  '/:id',
+  [
+    validateJWT,
+    check('id').notEmpty().withMessage('id is required').isMongoId().withMessage('id is not valid'),
+    check('country')
+      .optional()
+      .isLength({
+        max: 50,
+        min: 2,
+      })
+      .withMessage('country must be between 2 and 50 characters')
+      .custom((country) => {
+        if (!isNaN(country)) {
+          throw new Error('country should be a string');
+        }
+        return true;
+      }),
+    check('city')
+      .optional()
+      .isLength({
+        max: 50,
+        min: 2,
+      })
+      .withMessage('city must be between 2 and 50 characters')
+      .custom((city) => {
+        if (!isNaN(city)) {
+          throw new Error('city should be a string');
+        }
+        return true;
+      }),
+    check('district')
+      .optional()
+      .isLength({
+        max: 50,
+        min: 2,
+      })
+      .withMessage('district must be between 2 and 50 characters')
+      .custom((district) => {
+        if (!isNaN(district)) {
+          throw new Error('district should be a string');
+        }
+        return true;
+      }),
+    check('avenue')
+      .optional()
+      .isLength({
+        max: 50,
+        min: 2,
+      })
+      .withMessage('avenue must be between 2 and 50 characters')
+      .custom((avenue) => {
+        if (!isNaN(avenue)) {
+          throw new Error('avenue should be a string');
+        }
+        return true;
+      }),
+    check('number')
+      .optional()
+      .isInt({
+        min: 0,
+      })
+      .withMessage('number should be a positive number'),
+    check('department')
+      .optional()
+      .isInt({
+        min: 0,
+        max: 9999,
+      })
+      .withMessage('department should be a positive number'),
+    check('zipCode')
+      .optional()
+      .isLength({
+        max: 5,
+        min: 5,
+      })
+      .withMessage('zipCode must be 5 characters')
+      .isInt({
+        min: 0,
+      })
+      .withMessage('zipCode should be a positive number'),
+    validateInputs,
+  ],
+  updateAddress
 );
 
 module.exports = router;
