f536097c89d85b16d89b200f7f50e74013adf4a2
pool_1541 <pool_1541@hotmail.com>
2024-03-10 18:52:59 -0500

    feat(controllers): add basic controllers

--- src/controllers/address.controller.js
+++ src/controllers/address.controller.js
@@ -6,6 +6,27 @@ const getAddress = (req = request, res = response) => {
   });
 };
 
+const createAddress = (req = request, res = response) => {
+  return res.status(201).json({
+    message: 'Create Address',
+  });
+};
+
+const updateAddress = (req = request, res = response) => {
+  return res.status(200).json({
+    message: 'Update Address',
+  });
+};
+
+const deleteAddress = (req = request, res = response) => {
+  return res.status(200).json({
+    message: 'Delete Address',
+  });
+};
+
 module.exports = {
   getAddress,
+  createAddress,
+  updateAddress,
+  deleteAddress,
 };
