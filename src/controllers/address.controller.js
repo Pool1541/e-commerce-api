c82e224ddd112a04051f5f3a6373b4477fd75b80
pool_1541 <pool_1541@hotmail.com>
2024-03-17 17:06:34 -0500

    feat(controllers): add createAddress controller

--- src/controllers/address.controller.js
+++ src/controllers/address.controller.js
@@ -1,15 +1,48 @@
 const { request, response } = require('express');
+const Address = require('../models/address');
 
 const getAddress = (req = request, res = response) => {
+  const {} = req.body;
   return res.status(200).json({
     message: 'Get Address',
   });
 };
 
-const createAddress = (req = request, res = response) => {
-  return res.status(201).json({
-    message: 'Create Address',
-  });
+const createAddress = async (req = request, res = response) => {
+  try {
+    const { user, country, city, district, avenue, number, department, zipCode } = req.body;
+    const { _id: uid } = req.user;
+
+    const hasAddress = await Address.findOne({ user });
+
+    if (hasAddress)
+      return res.status(409).json({ error: ['User already has a registered address'] });
+
+    if (uid.toString() !== user.toString())
+      return res.status(401).json({ error: ['Not authorized'] });
+
+    const address = new Address({
+      user,
+      country,
+      city,
+      district,
+      avenue,
+      number,
+      department,
+      zipCode,
+    });
+
+    await address.save();
+
+    return res.status(201).json({
+      message: 'Address created succesfuly',
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      error: ['Something went wrong'],
+    });
+  }
 };
 
 const updateAddress = (req = request, res = response) => {
