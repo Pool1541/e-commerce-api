07a9c96e3edb17f045cf907347b0237fba91b7e9
pool_1541 <pool_1541@hotmail.com>
2024-03-17 17:34:35 -0500

    feat(controllers): add deleteAddress controller

--- src/controllers/address.controller.js
+++ src/controllers/address.controller.js
@@ -64,10 +64,27 @@ const updateAddress = (req = request, res = response) => {
   });
 };
 
-const deleteAddress = (req = request, res = response) => {
-  return res.status(200).json({
-    message: 'Delete Address',
-  });
+const deleteAddress = async (req = request, res = response) => {
+  try {
+    const { id } = req.params;
+    const { _id: uid } = req.user;
+
+    const address = await Address.findById(id);
+
+    if (!address) return res.status(404).json({ error: ['Address not found'] });
+    if (address.user.toString() !== uid.toString())
+      return res.status(401).json({ error: ['Not authorized'] });
+
+    await address.deleteOne();
+
+    return res.status(204).json({});
+  } catch (error) {
+    console.log(error);
+
+    return res.status(500).json({
+      error: ['Something went wrong'],
+    });
+  }
 };
 
 module.exports = {
