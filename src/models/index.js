60ab764712c5e996266550e6f46dcaab45817199
pool_1541 <pool_1541@hotmail.com>
2025-01-30 18:20:07 -0500

    feat(root): add index file to export all model modules

---
+++ src/models/index.js
@@ -0,0 +1,17 @@
+const Category = require('./category');
+const Product = require('./product');
+const Role = require('./role');
+const SubCategory = require('./subCategory');
+const User = require('./user');
+const PaymentMethod = require('./payment-method');
+const Address = require('./address');
+
+module.exports = {
+  Category,
+  Product,
+  Role,
+  SubCategory,
+  User,
+  PaymentMethod,
+  Address,
+}
